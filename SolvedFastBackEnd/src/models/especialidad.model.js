7f4ed91fd01684d83c1fc31bf87aa8abb00926bf
Daria Beatriz Lopez Franco <dlopezf@ulasalle.edu.pe>
2024-05-29 01:50:49 -0500

    Add app

--- SolvedFastBackEnd/src/models/especialidad.model.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const Schema = mongoose.Schema;
-
-const especialidadSchema = new Schema({
-    nombre_especialidad: { type: String, required: true },
-});
-
-export default mongoose.model("Especialidad", especialidadSchema);
--- SolvedFastBackEnd/src/routes/programar.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const router = Router();
-
-import { programar } from "../controllers/programar.controller.js";
-
-router.post("/programar", programar);
-
-export default router;
